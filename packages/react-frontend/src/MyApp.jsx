949a2c73d1c92d1080f022e6bcbb3320f1be234e
Danny-Kullman <dannykullman@gmail.com>
2026-01-09 16:53:29 -0800

    added form input

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,26 +1,11 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
 
   function removeOneCharacter(index) {
@@ -31,13 +16,14 @@ function MyApp() {
   }
 
   return (
-  <div className="container">
-    <Table
-      characterData={characters}
-      removeCharacter={removeOneCharacter}
-    />
-  </div>
-);
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form />
+    </div>
+  );
 
 }
 
